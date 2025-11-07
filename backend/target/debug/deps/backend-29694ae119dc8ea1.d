1c9cb00b3f62ee817526ee7ea2552f78fc75e059
SHREE-PRAVEEN <kpraveen206@yahoo.com>
2025-11-08 04:31:15 +0530

    frontend & backend setup

---
+++ backend/target/debug/deps/backend-29694ae119dc8ea1.d
@@ -0,0 +1,5 @@
+D:\PLANING FILES\cosmobharat technology\Roboveda\roboveda\backend\target\debug\deps\backend-29694ae119dc8ea1.d: src\main.rs
+
+D:\PLANING FILES\cosmobharat technology\Roboveda\roboveda\backend\target\debug\deps\libbackend-29694ae119dc8ea1.rmeta: src\main.rs
+
+src\main.rs:
