3571b5fd713d5df6ec06779ed045208383dbeb6d
SHREE-PRAVEEN <kpraveen206@yahoo.com>
2025-11-15 05:45:24 +0530

    change file architecture

---
+++ backend/target/debug/backend.d
@@ -0,0 +1,1 @@
+E:\PLANING\ FILES\cosmobharat\ technology\Roboveda\roboveda\backend\target\debug\backend.exe: E:\PLANING\ FILES\cosmobharat\ technology\Roboveda\roboveda\backend\src\main.rs
--- backend/target/debug/deps/backend.d
+++ backend/target/debug/deps/backend.d
@@ -1,5 +1,5 @@
-D:\PLANING FILES\cosmobharat technology\Roboveda\roboveda\backend\target\debug\deps\backend.d: src\main.rs
+E:\PLANING FILES\cosmobharat technology\Roboveda\roboveda\backend\target\debug\deps\backend.d: src\main.rs
 
-D:\PLANING FILES\cosmobharat technology\Roboveda\roboveda\backend\target\debug\deps\backend.exe: src\main.rs
+E:\PLANING FILES\cosmobharat technology\Roboveda\roboveda\backend\target\debug\deps\backend.exe: src\main.rs
 
 src\main.rs:
