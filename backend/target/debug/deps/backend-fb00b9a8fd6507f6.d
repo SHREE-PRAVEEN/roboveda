8f5823b779d0f9e89f0c6ee874aa0cca62597efa
PRAVEEN KUMAR <kpraveen206@yahoo.com>
2025-12-06 11:36:12 +0530

    Delete backend directory

--- backend/target/debug/deps/backend-fb00b9a8fd6507f6.d
+++
@@ -1,5 +0,0 @@
-D:\PLANING FILES\cosmobharat technology\Roboveda\roboveda\backend\target\debug\deps\backend-fb00b9a8fd6507f6.d: src\main.rs
-
-D:\PLANING FILES\cosmobharat technology\Roboveda\roboveda\backend\target\debug\deps\libbackend-fb00b9a8fd6507f6.rmeta: src\main.rs
-
-src\main.rs:
--- backend/target/debug/deps/backend.d
+++
@@ -1,5 +0,0 @@
-E:\PLANING FILES\cosmobharat technology\Roboveda\roboveda\backend\target\debug\deps\backend.d: src\main.rs
-
-E:\PLANING FILES\cosmobharat technology\Roboveda\roboveda\backend\target\debug\deps\backend.exe: src\main.rs
-
-src\main.rs:
